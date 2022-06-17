4a99176412187e33b5b5dc343c0c2d1372c54d3a
ahsanofficial <bega95091gmail.com>
2022-06-17 13:15:41 +0530

    first commit

---
+++ Nordstrom_clone_website/my_project/header/header.js
@@ -0,0 +1,274 @@
+function head(){
+    return `
+    <p id="topper">Give $150 in Gift Cards, earn a $25 Promo Card! Ends 6/19. Gift Cards & Restrictions<p>
+    <div id="top_head">
+    <div>
+        <h1>NORDSTROM</h1>
+    </div>
+    <div>
+       <span><i id="search_icon" class="fa-solid fa-magnifying-glass"></i> <input type="text" id="query" placeholder="Search for products or brands " ></span>
+    </div>
+    <div id="side_nav">
+        
+         <ul>
+             <li>
+                 <a href="">Sign in</a>
+                 <ul>
+                     <li>
+                     <a href="./login/signup/login.html"><button id="login">LOGIN</button></a>
+
+                     </li>
+                     <li><br>
+                     <a href="./login/signup/register.html"><button id="register">REGISTER</button></a>
+                     </li>
+                     <li>
+                         <a href="">Wish list</a>
+                     </li>
+                     <li>
+                         <a href="">Your Order</a>
+                     </li>
+                     <li>
+                         <a href="">Your referals</a>
+                     </li>
+                 </ul>
+             </li>
+             <li>
+                 <a href="./cart/cart2.html">cart</a>
+                 <ul>
+                     <li>
+                         <a href=""><p id="cart_item"></p></a>
+                     </li>
+                 </ul>
+             </li>
+         </ul>
+       
+    </div>
+</div><br>
+<hr>
+
+
+<div id="drop_down_menue">
+<div class="wrapper">
+<nav>
+    <ul>
+        <li class="drpdown"><a href="">Sale</a>
+            <ul class="menu-area">
+                <ul>
+                    <h4>Brands</h4>
+                    <li><a href="#">A to z Brands</a></li>
+                </ul>
+            </ul>
+        </li>
+        <li class="drpdown"><a href="./mothers_day.html">Women</a>
+            <ul class="menu-area">
+            <ul>
+                <h4>Shop by price</h4>
+                <li><a href="#">Under $50</a></li>
+                <li><a href="#">Under $100</a></li>
+                <li><a href="#">Luxury Gifts</a></li>
+                <li><a href="#">Fragrance Gifts</a></li>
+                <li><a href="#">Gifts And sets</a></li>
+                <li><a href="#">At home spa</a></li>
+            </ul>
+        </ul>
+        </li>
+        <li class="drpdown">
+            <a href="">Men</a>
+            <ul class="menu-area">
+            <ul>
+                <h4>Skin type routine</h4>
+                <li><a href="./index.html">Dry skinn routine</a></li>
+                <li><a href="">Normal skin routine</a></li>
+                <li><a href="">Only skin routine</a></li>
+                <li><a href="">Combination skin routine</a></li>
+                <li><a href="">Sensitive skin routine</a></li>
+                
+            </ul>
+            <ul>
+                <h4>Skin concern routine</h4>
+                <li><a href="">Acne routine</a></li>
+                <li><a href="">Fine Lines & Wrinkles Routine</a></li>
+                <li><a href="">Dark Spots & Discoloration Routine</a></li>
+                <li><a href="">Redness & Rosacea Routine</a></li>
+                <li><a href="">Dullness & Brightening Routine</a></li>
+               
+            </ul>
+            <ul>
+                <h4>Build a haire Routine</h4>
+                <li><a href="">Fine training haire routine</a></li>
+                <li><a href="">Healthy Scalp Routine</a></li>
+                <li><a href="">Colored Hair Routine</a></li>
+                <li><a href="">Dry, Damaged Hair Routine</a></li>
+                <li><a href="">Oily Hair Routine</a></li>
+                <li><a href="">Hair Styling Routine</a></li>
+                <li><a href=""></a></li>
+            </ul>
+        </ul>
+    </li>
+
+    <li class="drpdown"><a href="./Ahsan_skinCare/home.html">Kids</a>
+        <ul class="menu-area">
+            <ul>
+                <h4>Popular Categories</h4>
+                <li><a href="">View All Skin Care</a></li>
+                <li><a href="">New In</a></li>
+                <li><a href="">Clean Skincare</a></li>
+            </ul>
+            <ul>
+                <h4>By Product Type</h4>
+                <li><a href="./Ahsan_skinCare/Cleansers.html">Cleansers</a></li>
+                <li><a href="./Ahsan_skinCare/Moisturizers.html">Moisturizers</a></li>
+                <li><a href="./Ahsan_skinCare/Serums.html">Serums</a></li>
+                <li><a href="./Ahsan_skinCare/Eye Serum.html">Eye Serum</a></li>
+                <li><a href="./Ahsan_skinCare/Eye Care.html">Eye Care</a></li>
+               
+            </ul>
+            <ul>
+                <h4>By Ingredient</h4>
+                <li><a href="./Ahsan_skinCare/Vitamin C.html">Vitamin C</a></li>
+                <li><a href="./Ahsan_skinCare/AHA.html">AHA</a></li>
+                <li><a href="./Ahsan_skinCare/Caffeine.html">Caffeine</a></li>
+                <li><a href="./Ahsan_skinCare/Retinol.html">Retinol</a></li>
+                <li><a href="./Ahsan_skinCare/Lactic Acid.html">Lactic Acid</a></li>
+                <li><a href="./Ahsan_skinCare/Glycolic Acid.html">Glycolic Acid</a></li>
+                <li><a href="./Ahsan_skinCare/Azelaic Acid.html">Azelaic Acid</a></li>
+            </ul>
+            <ul>
+                <h4>By Specific Concern</h4>
+                <li><a href="./Ahsan_skinCare/Acne & Blemishes.html">Acne & Blemishes</a></li>
+                <li><a href="./Ahsan_skinCare/Fine Lines & Wrinkles">Fine Lines & Wrinkles</a></li>
+                <li><a href="./Ahsan_skinCare/Dark Circles.html">Dark Circles</a></li>
+                <li><a href="./Ahsan_skinCare/Dry Skin.html">Dry Skin</a></li>
+                <li><a href="./Ahsan_skinCare/Pregnancy Skincare.html">Pregnancy Skincare</a></li>
+                <li><a href="./Ahsan_skinCare/Sensitive Skin.html">Sensitive Skin</a></li>
+            </ul>
+            <ul>
+                <h4>Sunscreen & Suncare</h4>
+                <li><a href="./Ahsan_skinCare/After Sun.html">After Sun</a></li>
+                <li><a href="./Ahsan_skinCare/Tinted.html">Tinted</a></li>
+                <li><a href="./Ahsan_skinCare/Mineral.html">Mineral</a></li>
+                <li><a href="./Ahsan_skinCare/Eye Protection.html">Eye Protection</a></li>
+            </ul>
+        </ul>
+    </li>
+    <li class="drpdown"><a href="./selfcareproducts.html">Young Adult</a>
+    <ul class="menu-area">
+            <ul>
+                <h4>Popular Categories</h4>
+                <li><a href="">View All Cosmetics</a></li>
+                <li><a href="">View All Bath & Body</a></li>
+                <li><a href="">New In</a></li>
+                <li><a href="">Clean Bath & Body</a></li>
+                <li><a href="">At Home Spa</a></li>
+                <li><a href="">Gifts & Sets</a></li>
+                <li><a href="">Oral Care</a></li>
+
+            </ul>
+            <ul>
+                <h4>Bath & Shower</h4>
+                <li><a href="">Baths Oils/Soak</a></li>
+                <li><a href="">Bath Salts</a></li>
+                <li><a href="">Body Washes</a></li>
+                <li><a href="">Cleansing Bars</a></li>
+                <li><a href="">Exfoliators</a></li>
+               
+            </ul>
+            <ul>
+                <h4>Self Tanners</h4>
+                <li><a href="">Accessories</a></li>
+                <li><a href="">Pre-Tan Preparation</a></li>
+                <li><a href="">Body Tanners</a></li>
+                <li><a href="">Post Tanning</a></li>
+            </ul>
+            <ul>
+                <h4>Moisturizers</h4>
+                <li><a href="">Balms</a></li>
+                <li><a href="">Butters</a></li>
+                <li><a href="">Creams</a></li>
+                <li><a href="">Lotions</a></li>
+                <li><a href="">Lip Plumpers</a></li>
+                <li><a href="">Oils</a></li>
+            </ul>
+        </ul>
+    </li>
+    <li class="drpdown"><a href="./makup.html">Activewear</a>
+        <ul class="menu-area">
+            <ul>
+                <h4>Popular Categories</h4>
+                <li><a href="">View All Cosmetics</a></li>
+                <li><a href="">New In</a></li>
+                <li><a href="">Clean Makeup</a></li>
+            </ul>
+            <ul>
+                <h4>Eyes</h4>
+                <li><a href="">Eye Liners</a></li>
+                <li><a href="">Lash & Brow Enhancers</a></li>
+                <li><a href="">Eye Shadows</a></li>
+                <li><a href="">Mascaras</a></li>
+                <li><a href="">Brow Pencils</a></li>
+               
+            </ul>
+            <ul>
+                <h4>Face</h4>
+                <li><a href="">BB & CC Cream</a></li>
+                <li><a href="">Blushers</a></li>
+                <li><a href="">Bronzers</a></li>
+                <li><a href="">Color Correctors</a></li>
+                <li><a href="">Concealers</a></li>
+                <li><a href="">Contour</a></li>
+                <li><a href="">Face Powders</a></li>
+            </ul>
+            <ul>
+                <h4>Lips</h4>
+                <li><a href="">Lipsticks</a></li>
+                <li><a href="">Lip Balms</a></li>
+                <li><a href="">Lip Glosses</a></li>
+                <li><a href="">Lip Liners</a></li>
+                <li><a href="">Lip Plumpers</a></li>
+                <li><a href="">Liquid Lipsticks</a></li>
+            </ul>
+        </ul>
+    </li>
+    <li class="drpdown"><a href="./self.html">Home</a>
+        <ul class="menu-area">
+            <ul>
+                <h4>Popular Categories</h4>
+                <li><a href="selfcareproducts.html">View All Fragrance</a></li>
+                <li><a href="">New In</a></li>
+                <li><a href="">At-Home Spa</a></li>
+                <li><a href="">View All Wellness Products</a></li>
+            </ul>
+            <ul>
+                <h4>Sexual Wellness</h4>
+                <li><a href="">Devices</a></li>
+                <li><a href="">Skin Care</a></li>
+            </ul>
+            <ul>
+                <h4>Supplements</h4>
+                <li><a href="">Skin</a></li>
+                <li><a href="">Hair</a></li>
+                <li><a href="">Immune Boosting</a></li>
+                <li><a href="">Health & Wellbeing</a></li>
+                <li><a href="">Weight Loss</a></li>
+            </ul>
+            <ul>
+                <h4>Clean Beauty</h4>
+                <li><a href="">Clean Skincare</a></li>
+                <li><a href="">Clean Haircare</a></li>
+                <li><a href="">Clean Makeup</a></li>
+                <li><a href="">Clean Bath & Body</a></li>
+            </ul>
+        </ul>
+    </li>
+    </ul>
+</nav>
+</div>
+<br><br><br>
+<p class="add1">Shop what you love—faster and easier.<p>
+
+
+    `
+
+}
+
+export default head
